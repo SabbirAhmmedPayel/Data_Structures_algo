c1e81e0f106fb0834c533b376c01aa05b1bf776d
SabbirAhmmedPayel <159970924+SabbirAhmmedPayel@users.noreply.github.com>
2025-05-03 10:29:54 +0600

    Update bellman_ford.cpp

--- graph/bellman_ford.cpp
+++ graph/bellman_ford.cpp
@@ -5,23 +5,23 @@
 using namespace std;
 
 struct Edge {
-    int source, destination, weight;
+    int u, v, w;
 };
 
 void bellmanFord(vector<Edge>& graph, int vertices, int source) {
-    vector<int> distance(vertices, INT_MAX);
-    distance[source] = 0;
+    vector<int> dis(vertices, INT_MAX);
+    dis[source] = 0;
 
     for (int i = 0; i < vertices - 1; ++i) {
         for (const Edge& e : graph) {
-            if (distance[e.source] != INT_MAX && distance[e.source] + e.weight < distance[e.destination]) {
-                distance[e.destination] = distance[e.source] + e.weight;
+            if (dis[e.u] != INT_MAX && dis[e.u] + e.w < dis[e.v]) {
+                dis[e.v] = dis[e.u] + e.w;
             }
         }
     }
 
     for (const Edge& e : graph) {
-        if (distance[e.source] != INT_MAX && distance[e.source] + e.weight < distance[e.destination]) {
+        if (dis[e.u] != INT_MAX && dis[e.u] + e.w < dis[e.v]) {
             cout << "Graph contains negative weight cycle\n";
             return;
         }
@@ -29,19 +29,18 @@ void bellmanFord(vector<Edge>& graph, int vertices, int source) {
 
     cout << "Vertex   Distance from Source\n";
     for (int i = 0; i < vertices; ++i) {
-        cout << i << "\t\t" << distance[i] << "\n";
+        cout << i << "\t\t" << dis[i] << "\n";
     }
 }
 
 int main() {
     int vertices = 6;
     vector<Edge> graph = {
-        { 0, 1, 5 }, { 0, 2, 7 }, { 1, 2, 3 },
-        { 1, 3, 4 }, { 1, 4, 6 }, { 3, 4, -1 },
-        { 3, 5, 2 }, { 4, 5, -3 }
+        {0, 1, 5}, {0, 2, 7}, {1, 2, 3},
+        {1, 3, 4}, {1, 4, 6}, {3, 4, -1},
+        {3, 5, 2}, {4, 5, -3}
     };
 
     bellmanFord(graph, vertices, 0);
-
     return 0;
 }
