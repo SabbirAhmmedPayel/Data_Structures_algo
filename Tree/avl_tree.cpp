fdaae2547e18257a8cf5bcac686a00e01ba992b3
SabbirAhmmedPayel <159970924+SabbirAhmmedPayel@users.noreply.github.com>
2025-06-06 15:06:02 +0600

    Update avl_tree.cpp

--- Tree/avl_tree.cpp
+++ Tree/avl_tree.cpp
@@ -10,98 +10,190 @@ struct Node {
     Node(int val) : key(val), left(nullptr), right(nullptr), height(1) {}
 };
 
-int getHeight(Node* node) {
-    return node ? node->height : 0;
-}
+class AVLTree {
+private:
+    Node* root;
 
-int getBalance(Node* node) {
-    return node ? getHeight(node->left) - getHeight(node->right) : 0;
-}
+    int getHeight(Node* node) {
+        return node ? node->height : 0;
+    }
 
-void updateHeight(Node* node) {
-    node->height = 1 + max(getHeight(node->left), getHeight(node->right));
-}
+    int getBalance(Node* node) {
+        return node ? getHeight(node->left) - getHeight(node->right) : 0;
+    }
 
-Node* rotateRight(Node* y) {
-    Node* x = y->left;
-    Node* T2 = x->right;
-    x->right = y;
-    y->left = T2;
-    updateHeight(y);
-    updateHeight(x);
-    return x;
-}
+    void updateHeight(Node* node) {
+        node->height = 1 + max(getHeight(node->left), getHeight(node->right));
+    }
 
-Node* rotateLeft(Node* x) {
-    Node* y = x->right;
-    Node* T2 = y->left;
-    y->left = x;
-    x->right = T2;
-    updateHeight(x);
-    updateHeight(y);
-    return y;
-}
+    Node* rotateRight(Node* y) {
+        Node* x = y->left;
+        Node* T2 = x->right;
+        x->right = y;
+        y->left = T2;
+        updateHeight(y);
+        updateHeight(x);
+        return x;
+    }
 
-Node* insert(Node* node, int key) {
-    if (!node)
-        return new Node(key);
+    Node* rotateLeft(Node* x) {
+        Node* y = x->right;
+        Node* T2 = y->left;
+        y->left = x;
+        x->right = T2;
+        updateHeight(x);
+        updateHeight(y);
+        return y;
+    }
 
-    if (key < node->key)
-        node->left = insert(node->left, key);
-    else if (key > node->key)
-        node->right = insert(node->right, key);
-    else
+    Node* insert(Node* node, int key) {
+        if (!node)
+            return new Node(key);
+
+        if (key < node->key)
+            node->left = insert(node->left, key);
+        else if (key > node->key)
+            node->right = insert(node->right, key);
+        else
+            return node;
+
+        updateHeight(node);
+        int balance = getBalance(node);
+
+        if (balance > 1 && key < node->left->key)
+            return rotateRight(node);
+
+        if (balance < -1 && key > node->right->key)
+            return rotateLeft(node);
+
+        if (balance > 1 && key > node->left->key) {
+            node->left = rotateLeft(node->left);
+            return rotateRight(node);
+        }
+
+        if (balance < -1 && key < node->right->key) {
+            node->right = rotateRight(node->right);
+            return rotateLeft(node);
+        }
+
         return node;
-
-    updateHeight(node);
-    int balance = getBalance(node);
-
-    if (balance > 1 && key < node->left->key)
-        return rotateRight(node);
-    if (balance < -1 && key > node->right->key)
-        return rotateLeft(node);
-    if (balance > 1 && key > node->left->key) {
-        node->left = rotateLeft(node->left);
-        return rotateRight(node);
-    }
-    if (balance < -1 && key < node->right->key) {
-        node->right = rotateRight(node->right);
-        return rotateLeft(node);
     }
 
-    return node;
-}
-
-void inorder(Node* root) {
-    if (root) {
-        inorder(root->left);
-        cout << root->key << " ";
-        inorder(root->right);
+    Node* minValueNode(Node* node) {
+        Node* current = node;
+        while (current->left)
+            current = current->left;
+        return current;
     }
-}
 
-void visualizeTree(Node* root, int space = 0, int height = 10) {
-    if (!root) return;
+    Node* deleteNode(Node* root, int key) {
+        if (!root)
+            return root;
 
-    space += height;
+        if (key < root->key)
+            root->left = deleteNode(root->left, key);
+        else if (key > root->key)
+            root->right = deleteNode(root->right, key);
+        else {
+            if (!root->left || !root->right) {
+                Node* temp = root->left ? root->left : root->right;
+                if (!temp) {
+                    temp = root;
+                    root = nullptr;
+                } else
+                    *root = *temp;
+                delete temp;
+            } else {
+                Node* temp = minValueNode(root->right);
+                root->key = temp->key;
+                root->right = deleteNode(root->right, temp->key);
+            }
+        }
 
-    visualizeTree(root->right, space);
-    cout << endl;
-    cout << setw(space) << root->key << "\n";
-    visualizeTree(root->left, space);
-}
+        if (!root)
+            return root;
+
+        updateHeight(root);
+        int balance = getBalance(root);
+
+        if (balance > 1 && getBalance(root->left) >= 0)
+            return rotateRight(root);
+
+        if (balance > 1 && getBalance(root->left) < 0) {
+            root->left = rotateLeft(root->left);
+            return rotateRight(root);
+        }
+
+        if (balance < -1 && getBalance(root->right) <= 0)
+            return rotateLeft(root);
+
+        if (balance < -1 && getBalance(root->right) > 0) {
+            root->right = rotateRight(root->right);
+            return rotateLeft(root);
+        }
+
+        return root;
+    }
+
+    void inorder(Node* node) {
+        if (node) {
+            inorder(node->left);
+            cout << node->key << " ";
+            inorder(node->right);
+        }
+    }
+
+    void visualize(Node* node, int space = 0, int height = 10) {
+        if (!node) return;
+
+        space += height;
+        visualize(node->right, space);
+        cout << endl << setw(space) << node->key << "\n";
+        visualize(node->left, space);
+    }
+
+public:
+    AVLTree() : root(nullptr) {}
+
+    void insert(int key) {
+        root = insert(root, key);
+    }
+
+    void remove(int key) {
+        root = deleteNode(root, key);
+    }
+
+    void displayInorder() {
+        inorder(root);
+        cout << endl;
+    }
+
+    void displayTree() {
+        visualize(root);
+    }
+};
 
 int main() {
-    Node* root = nullptr;
+    AVLTree tree;
+
     int keys[] = {10, 20, 30, 40, 50, 25};
-
     for (int key : keys)
-        root = insert(root, key);
+        tree.insert(key);
 
-    cout << "In-order traversal of the AVL Tree:\n";
-    inorder(root);
-    cout << "\n\nVisual representation of AVL Tree:\n";
-    visualizeTree(root);
+    cout << "In-order traversal of AVL Tree:\n";
+    tree.displayInorder();
+
+    cout << "\nVisual representation of AVL Tree:\n";
+    tree.displayTree();
+
+    cout << "\n\nDeleting node 40...\n";
+    tree.remove(40);
+
+    cout << "\nIn-order after deletion:\n";
+    tree.displayInorder();
+
+    cout << "\nVisual representation after deletion:\n";
+    tree.displayTree();
 
     return 0;
 }
