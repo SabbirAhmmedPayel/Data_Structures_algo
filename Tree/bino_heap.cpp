68fe6e2ffa26553e4f693c35f11eefffb041ed2a
SabbirAhmmedPayel <ahommed23.79@gmail.com>
2025-06-26 01:45:48 +0600

    ok

--- Tree/bino_heap.cpp
+++ Tree/bino_heap.cpp
@@ -1,101 +1,196 @@
 #include <iostream>
-#include <vector>
+#include <list>
+#include <cmath>
 #include <climits>
+#include<vector>
+#include <algorithm>
+
 using namespace std;
 
 class Node {
 public:
-    int key, degree;
-    Node *parent, *child, *sibling;
-    Node(int k) : key(k), degree(0), parent(nullptr), child(nullptr), sibling(nullptr) {}
+    int value;
+    Node* parent;
+    list<Node*> children;
+    int degree;
+
+    Node(int val) {
+        value = val;
+        parent = nullptr;
+        degree = 0;
+    }
 };
 
 class BinomialHeap {
-    vector<Node*> roots;
+public:
+    list<Node*> trees;
+    Node* min_node;
+    int count;
 
-    Node* mergeTrees(Node* a, Node* b) {
-        if (a->key > b->key) swap(a, b);
-        b->parent = a;
-        b->sibling = a->child;
-        a->child = b;
-        a->degree++;
-        return a;
+    BinomialHeap() {
+        min_node = nullptr;
+        count = 0;
     }
 
-    vector<Node*> unionHeaps(vector<Node*> &h1, vector<Node*> &h2) {
-        vector<Node*> res;
-        int i = 0, j = 0;
-        while (i < (int)h1.size() && j < (int)h2.size()) {
-            if (h1[i]->degree < h2[j]->degree) res.push_back(h1[i++]);
-            else res.push_back(h2[j++]);
+    bool is_empty() {
+        return min_node == nullptr;
+    }
+
+    void insert(int value) {
+        Node* node = new Node(value);
+        BinomialHeap temp;
+        temp.trees.push_back(node);
+        temp.count = 1;
+        merge(temp);
+    }
+
+    int get_min() {
+        if (min_node == nullptr) return INT_MIN;
+        return min_node->value;
+    }
+
+    int extract_min() {
+        if (min_node == nullptr) return INT_MIN;
+
+        trees.remove(min_node);
+
+        BinomialHeap temp;
+        for (Node* child : min_node->children) {
+            child->parent = nullptr;
+            temp.trees.push_back(child);
         }
-        while (i < (int)h1.size()) res.push_back(h1[i++]);
-        while (j < (int)h2.size()) res.push_back(h2[j++]);
-        return res;
+        temp.count = pow(2, min_node->degree);
+
+        int minVal = min_node->value;
+        delete min_node;
+
+        merge(temp);
+        count--;
+
+        return minVal;
     }
 
-    vector<Node*> adjust(vector<Node*> &heap) {
-        if (heap.size() <= 1) return heap;
-        vector<Node*> res;
-        int i = 0;
-        while (i < (int)heap.size()) {
-            if (i + 1 < (int)heap.size() && heap[i]->degree == heap[i + 1]->degree) {
-                if (i + 2 < (int)heap.size() && heap[i + 1]->degree == heap[i + 2]->degree) {
-                    res.push_back(heap[i++]);
-                } else {
-                    heap[i + 1] = mergeTrees(heap[i], heap[i + 1]);
-                    i++;
-                }
-            } else {
-                res.push_back(heap[i++]);
+    void merge(BinomialHeap& other) {
+        for (Node* node : other.trees) {
+            trees.push_back(node);
+        }
+        count += other.count;
+        consolidate();
+    }
+
+    void consolidate() {
+        if (trees.empty()) {
+            min_node = nullptr;
+            return;
+        }
+
+        int max_degree = (count > 0) ? static_cast<int>(log2(count)) + 2 : 2;
+        vector<Node*> degree_map(max_degree, nullptr);
+
+        for (auto it = trees.begin(); it != trees.end();) {
+            Node* current = *it;
+            int d = current->degree;
+
+            while (degree_map[d] != nullptr) {
+                Node* other = degree_map[d];
+                degree_map[d] = nullptr;
+
+                auto remove_it = find(trees.begin(), trees.end(), other);
+                if (remove_it != trees.end()) trees.erase(remove_it);
+
+                if (current->value > other->value)
+                    swap(current, other);
+
+                link(current, other);
+                d = current->degree;
+            }
+
+            degree_map[d] = current;
+            ++it;
+        }
+
+        trees.clear();
+        min_node = nullptr;
+
+        for (Node* node : degree_map) {
+            if (node != nullptr) {
+                trees.push_back(node);
+                if (min_node == nullptr || node->value < min_node->value)
+                    min_node = node;
             }
         }
-        return res;
     }
 
-public:
-    void insert(int key) {
-        Node* node = new Node(key);
-        vector<Node*> temp = {node};
-        roots = unionHeaps(roots, temp);
-        roots = adjust(roots);
+    void link(Node* parent, Node* child) {
+        child->parent = parent;
+        parent->children.push_back(child);
+        parent->degree++;
     }
 
-    int extractMin() {
-        if (roots.empty()) return INT_MIN;
-        int minIndex = 0;
-        for (int i = 1; i < (int)roots.size(); ++i)
-            if (roots[i]->key < roots[minIndex]->key)
-                minIndex = i;
+    void decrease_key(Node* node, int new_value) {
+        if (new_value > node->value) return;
+        node->value = new_value;
+        Node* parent = node->parent;
 
-        Node* minNode = roots[minIndex];
-        roots.erase(roots.begin() + minIndex);
-
-        vector<Node*> childHeap;
-        Node* cur = minNode->child;
-        while (cur) {
-            Node* next = cur->sibling;
-            cur->sibling = nullptr;
-            cur->parent = nullptr;
-            childHeap.insert(childHeap.begin(), cur);
-            cur = next;
+        while (parent != nullptr && node->value < parent->value) {
+            swap(node->value, parent->value);
+            node = parent;
+            parent = node->parent;
         }
-        roots = unionHeaps(roots, childHeap);
-        roots = adjust(roots);
 
-        int minKey = minNode->key;
-        delete minNode;
-        return minKey;
+        if (min_node == nullptr || node->value < min_node->value)
+            min_node = node;
     }
+
+    void delete_node(Node* node) {
+        decrease_key(node, INT_MIN);
+        extract_min();
+    }
+
+    int size() {
+        return count;
+    }
+
+    void print_tree(Node* node, int depth) {
+        for (int i = 0; i < depth; i++) cout << "  ";
+        cout << node->value << "\n";
+        for (Node* child : node->children) {
+            print_tree(child, depth + 1);
+        }
+    }
+
+    void printHeap() {
+        cout << "Heap Trees:\n";
+        for (Node* tree : trees) {
+            print_tree(tree, 0);
+            cout << "------\n";
+        }
+    }
+
+   
 };
 
 int main() {
-    BinomialHeap bh;
-    bh.insert(10);
-    bh.insert(2);
-    bh.insert(7);
-    bh.insert(1);
+    BinomialHeap heap;
 
-    cout << "Min: " << bh.extractMin() << "\n";
-    cout << "Min: " << bh.extractMin() << "\n";
+    heap.insert(10);
+    heap.insert(3);
+    heap.insert(7);
+    heap.insert(6);
+    heap.insert(15);
+    heap.insert(1);
+
+    cout << "Initial Heap:\n";
+    heap.printHeap();
+
+    cout << "Extracted min: " << heap.extract_min() << "\n";
+    cout << "Heap after extract_min:\n";
+    heap.printHeap();
+
+    cout << "Insert 2 and 8\n";
+    heap.insert(2);
+    heap.insert(8);
+    heap.printHeap();
+
+    return 0;
 }
